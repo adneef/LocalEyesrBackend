0f387dcb282dfed0e49f2bf804673c01a5be95f4
Adam Neef <adneef@gmail.com>
2017-11-18 19:37:11 -0700

    trying to fix heroku deployment

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,7 @@ passport.deserializeUser((dbUser, done) => {
 passport.use(new GoogleStrategy({
   clientID: `${process.env.GOOGLE_CLIENT_ID}`,
   clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  callbackURL: "http://localhost:5000/auth/google/callback",
+  callbackURL: "/auth/google/callback",
   passReqToCallback: true
 }, function(request, accessToken, refreshToken, profile, done) {
   let id = profile.id
