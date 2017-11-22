d7fd3eafce7b1d161d24163cff37ac83fb97334e
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-21 18:02:13 -0700

    twitter

--- routes/twitter.js
+++ routes/twitter.js
@@ -110,30 +110,30 @@ router.get('/denver', function(req, res, next) {
 
 
 // most recent tweets for top trend
-router.get('/tweets', function(req, res, next) {
-  console.log('query term', req.query.term);
-  t.get('search/tweets', { q: `${req.query.term}`, count: 5 }, gotData)
-  // filter data from twitter API call
-  function gotData(err, data, response) {
-    let filteredTweets = data.statuses.slice(1, 5).map(item => {
-      if (item.entities.hashtags.length === 0) {
-        return {
-          created_at: item.created_at,
-          text: item.text,
-          hashtags: 'none'
-        }
-      }
-      let hashtags = item.entities.hashtags[0].text
-      return {
-        created_at: item.created_at,
-        text: item.text,
-        hashtags: hashtags
-      }
-    })
-    console.log(filteredTweets);
-    return res.send(filteredTweets)
-  }
-})
+// router.get('/tweets', function(req, res, next) {
+//   console.log('query term', req.query.term);
+//   t.get('search/tweets', { q: `${req.query.term}`, count: 5 }, gotData)
+//   // filter data from twitter API call
+//   function gotData(err, data, response) {
+//     let filteredTweets = data.statuses.slice(1, 5).map(item => {
+//       if (item.entities.hashtags.length === 0) {
+//         return {
+//           created_at: item.created_at,
+//           text: item.text,
+//           hashtags: 'none'
+//         }
+//       }
+//       let hashtags = item.entities.hashtags[0].text
+//       return {
+//         created_at: item.created_at,
+//         text: item.text,
+//         hashtags: hashtags
+//       }
+//     })
+//     console.log(filteredTweets);
+//     return res.send(filteredTweets)
+//   }
+// })
 
 
 module.exports = router;
